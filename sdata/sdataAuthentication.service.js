489f007548ec0f1d2cd8a57db501b4ecb02c7e26
Nicolas Galler <ngaller@gmail.com>
2017-04-20 06:18:11 -0400

    Fix sdata auth tests

--- sdata/sdataAuthentication.service.js
+++ sdata/sdataAuthentication.service.js
@@ -1,68 +1,75 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    angular.module('blocks.sdata')
-        .factory('sdataAuthenticationService', sdataAuthenticationService);
+  angular.module('blocks.sdata')
+    .factory('sdataAuthenticationService', sdataAuthenticationService);
 
-    function sdataAuthenticationService($q, sdataService) {
-        var service = {
-            isAuthenticated: isAuthenticated,
-            clearAuthentication: clearAuthentication,
-            login: login,
-            hasRole: hasRole,
-            hasSecuredAction: hasSecuredAction
-        };
+  function sdataAuthenticationService($q, sdataService) {
+    var service = {
+      isAuthenticated: isAuthenticated,
+      clearAuthentication: clearAuthentication,
+      login: login,
+      hasRole: hasRole,
+      hasSecuredAction: hasSecuredAction
+    };
 
-        //////////
+    //////////
 
-        var _isAuthenticated = false;
-        var _roles = {};
-        var _securedActions = {};
-        var _userId = null;
+    var _isAuthenticated = false;
+    var _roles = {};
+    var _securedActions = {};
+    var _userId = null;
 
-        function login(username, password) {
-            sdataService.setAuthenticationParameters(username, password);
-            return sdataService.executeRequest('/slx/system/-/$service/getCurrentUser?format=json', 'POST', {})
-                .then(function (data) {
-                    _isAuthenticated = true;
-                    cacheUserRoles(data.response);
-                    _userId = data.response.userId.trim();
-                }, function () {
-                    _isAuthenticated = false;
-                    return $q.reject('Login failed');
-                });
-        }
-
-        function clearAuthentication() {
-            sdataService.setAuthenticationParameters(null, null);
-            _isAuthenticated = false;
-        }
-
-        function isAuthenticated() {
-            return _isAuthenticated;
-        }
-
-        function hasSecuredAction(sa) {
-            return isAuthenticated() && (_userId == 'ADMIN' || _securedActions[sa]);
-        }
-
-        function hasRole(role) {
-            return isAuthenticated() && (_userId == 'ADMIN' || _roles[role]);
-        }
-
-        function cacheUserRoles(userData) {
-            var r = userData.roles;
-            _roles = {};
-            for (var i = 0; i < r.length; i++) {
-                _roles[r[i]] = true;
-            }
-            var sa = userData.securedActions;
-            _securedActions = {};
-            for (var j = 0; j < sa.length; j++) {
-                _securedActions[sa[j]] = true;
-            }
-        }
-
-        return service;
+    function login(username, password) {
+      sdataService.setAuthenticationParameters(username, password);
+      return sdataService.executeRequest('/slx/system/-/$service/getCurrentUser?format=json', 'POST', {})
+        .then(function (data) {
+          _isAuthenticated = true;
+          console.log('GOT DATA', data);
+          debugger
+          // this returns the data in a response property
+          cacheUserRoles(data.response);
+          _userId = data.response.userId.trim();
+        }, function () {
+          _isAuthenticated = false;
+          return $q.reject('Login failed');
+        });
     }
+
+    function clearAuthentication() {
+      sdataService.setAuthenticationParameters(null, null);
+      _isAuthenticated = false;
+    }
+
+    function isAuthenticated() {
+      return _isAuthenticated;
+    }
+
+    function hasSecuredAction(sa) {
+      return isAuthenticated() && (_userId == 'ADMIN' || _securedActions[sa]);
+    }
+
+    function hasRole(role) {
+      return isAuthenticated() && (_userId == 'ADMIN' || _roles[role]);
+    }
+
+    function cacheUserRoles(userData) {
+      var r = userData.roles;
+      _roles = {};
+      if(r) {
+        for (var i = 0; i < r.length; i++) {
+          _roles[r[i]] = true;
+        }
+      }
+      var sa = userData.securedActions;
+      _securedActions = {};
+      if(sa) {
+        for (var j = 0; j < sa.length; j++) {
+          _securedActions[sa[j]] = true;
+        }
+      }
+    }
+
+    return service;
+  }
 })();
--- sdata/tests/authentication.service.spec.js
+++ sdata/tests/authentication.service.spec.js
@@ -1,45 +1,83 @@
 /* jshint -W117, -W030 */
 describe('blocks.sdata', function () {
-    describe('sdataAuthenticationService', function () {
-        beforeEach(function () {
-            // use asyncModule otherwise we cannot return promises from tests
-            bard.asyncModule('blocks.sdata', function (sdataServiceProvider) {
-                sdataServiceProvider.configure({sdataUri: 'fake server'});
-            });
+  describe('sdataAuthenticationService', function () {
+    beforeEach(function () {
+      // use asyncModule otherwise we cannot return promises from tests
+      bard.asyncModule('blocks.sdata', function (sdataServiceProvider) {
+        sdataServiceProvider.configure({sdataUri: 'fake server'});
+      });
 
-            bard.inject(this, 'sdataAuthenticationService', 'sdataService', '$q', '$rootScope');
+      bard.inject(this, 'sdataAuthenticationService', 'sdataService', '$q', '$rootScope');
 
-        });
+    });
 
-        it('should login to sdata', function () {
-            bard.mockService(sdataService, {
-                executeRequest: $q.when([])
-            });
-            return sdataAuthenticationService.login('admin', '').then(function () {
-                expect(sdataAuthenticationService.isAuthenticated()).to.be.true;
-            });
-        });
+    it('should login to sdata', function () {
+      bard.mockService(sdataService, {
+        executeRequest: $q.when({
+          response: {
+            userId: 'ADMIN',
+            roles: []
+          }
+        })
+      });
+      return sdataAuthenticationService.login('admin', '').then(function () {
+        expect(sdataAuthenticationService.isAuthenticated()).to.be.true;
+      });
+    });
 
-        it('should reject login error with "login failed"', function () {
-            bard.mockService(sdataService, {
-                executeRequest: $q.reject('Foo')
-            });
-            return sdataAuthenticationService.login('admin', '').then(function () {
-                assert.fail(0, 0, 'The login call should not succeed');
-            }, function (error) {
-                expect(error).to.equal('Login failed');
-                expect(sdataAuthenticationService.isAuthenticated()).to.be.false;
-            });
-        });
+    it('should reject login error with "login failed"', function () {
+      bard.mockService(sdataService, {
+        executeRequest: $q.reject('Foo')
+      });
+      return sdataAuthenticationService.login('admin', '').then(function () {
+        assert.fail(0, 0, 'The login call should not succeed');
+      }, function (error) {
+        expect(error).to.equal('Login failed');
+        expect(sdataAuthenticationService.isAuthenticated()).to.be.false;
+      });
+    });
 
-        it('should logout when logout is called', function () {
-            bard.mockService(sdataService, {
-                executeRequest: $q.when([])
-            });
-            return sdataAuthenticationService.login('admin', '').then(function () {
-                sdataAuthenticationService.clearAuthentication();
-                expect(sdataAuthenticationService.isAuthenticated()).to.be.false;
-            });
-        });
+    it('should logout when logout is called', function () {
+      bard.mockService(sdataService, {
+        executeRequest: $q.when({
+          response: {
+            userId: 'ADMIN',
+            roles: []
+          }
+        })
+      });
+      return sdataAuthenticationService.login('admin', '').then(function () {
+        sdataAuthenticationService.clearAuthentication();
+        expect(sdataAuthenticationService.isAuthenticated()).to.be.false;
+      });
+    });
+
+    it('should capture user roles when logging in', function() {
+      bard.mockService(sdataService, {
+        executeRequest: $q.when({
+          response: {
+            userId: 'ADMIN',
+            roles: ['Standard User']
+          }
+        })
+      });
+      return sdataAuthenticationService.login('admin', '').then(function () {
+        expect(sdataAuthenticationService.hasRole('Standard User')).to.be.true
+      });
     })
+
+    it('should capture secured actions when logging in', function() {
+      bard.mockService(sdataService, {
+        executeRequest: $q.when({
+          response: {
+            userId: 'ADMIN',
+            securedActions: ['Entities/Account/Add']
+          }
+        })
+      });
+      return sdataAuthenticationService.login('admin', '').then(function () {
+        expect(sdataAuthenticationService.hasSecuredAction('Entities/Account/Add')).to.be.true
+      });
+    })
+  })
 })
